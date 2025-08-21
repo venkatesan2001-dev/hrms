0084a0c23a79e185e2f9ee6d43d499f6e2f304aa
Venkatesan K <venkatesan2001official@gmail.com>
2025-08-21 14:07:47 +0530

    Bug fixed

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,7 +1,9 @@
 import { all, call, put, takeLatest } from "redux-saga/effects";
 import axios from "axios";
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/",
+  baseURL:
+    import.meta.env.VITE_API_BASE_URL ||
+    "https://hrms-backend-beige.vercel.app/",
 });
 import {
   fetchUsers,
